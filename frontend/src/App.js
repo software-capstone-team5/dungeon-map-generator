dbd75cba6a0b2b56d041e450d2916b8d13e0d69f
bojotamara <tamarabojovic@hotmail.com>
2021-03-29 17:38:59 -0600

    #1 Added app bar, contains app title and login button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
-import { Grid } from '@material-ui/core';
+import { Grid, AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import ConfigurationEditor from "./ConfigurationEditor";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
-     <div className="App">
+    <div className="App">
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Dungeon Map Generator
+            </Typography>
+            <Button color="inherit">Login</Button> {/* TODO: Change this button depending on login state */}
+          </Toolbar>
+        </AppBar>
+      </div>
       <Grid
         container
         direction="row"
