6e82907382e5f15c69d2aa41390f7d5bbca6c1ff
Jacob Wong <jdw2@ualberta.ca>
2021-04-10 23:09:35 -0600

    #83 Update Backend route for getAllConfig and getConfigByID

--- backend/util.py
+++ backend/util.py
@@ -12,7 +12,23 @@ def getDBID(data, collection_ref):
         data['id'] = document.id
     return data, document
 
-def getConfigReferences(references):
+def getConfigReferences(config):
+    configDict = config.to_dict()
+    corridorRefs = configDict['corridorCategories']['objects']
+    configDict['corridorCategories']['objects'] = getSubReferences(corridorRefs)
+
+    roomRefs = configDict['roomCategories']['objects']
+    configDict['roomCategories']['objects'] = getSubReferences(roomRefs)
+
+    defaultCorridorRef = configDict['defaultCorridorCategory']
+    configDict['defaultCorridorCategory'] = getSubReferences([defaultCorridorRef])[0]
+
+    defaultRoomRef = configDict['defaultRoomCategory']
+    configDict['defaultRoomCategory'] = getSubReferences([defaultRoomRef])[0]
+
+    return configDict
+
+def getSubReferences(references):
     result = []
     for reference in references:
         category = reference.get().to_dict()
